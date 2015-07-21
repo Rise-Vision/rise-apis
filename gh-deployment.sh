34345ade5954039dfd03160a898532249be5fdaa
rodrigopavezi <rodrigopavezi@gmail.com>
2015-07-21 10:52:23 -0300

    Fix deployment script

--- gh-deployment.sh
+++ gh-deployment.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/expect -f
 spawn yo polymer:gh
+expect "We're constantly looking for ways to make yo better!"
+send -- "Y\n"
 expect "What is your GitHub username?"
 send -- "Rise-Vision\n"
 expect "What is your element's name: (rise-apis)"
